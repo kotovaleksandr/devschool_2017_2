8d86859887211f78ffd57c0bfc1a2d2d7ccce692
kotov.a <kotov.a@digdes.com>
2017-10-25 11:14:46 +0300

    log sample

---
+++ TestLog/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace TestLog
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var logger = LogManager.GetCurrentClassLogger();
+			logger.Info("Инфо параметр: {0}", DateTime.Now);
+
+			//бизнес-логика
+
+			logger.Error("Ошибка");
+			Console.ReadKey();
+		}
+	}
+}
